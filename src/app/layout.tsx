cf634d4948431ce145a4fc522128bb965bbc4772
Vino Chelo <vinochelo1986@gmail.com>
2025-10-30 20:10:04 +0000

    necesito instalar analitycs de vercel Install our package
    Start by ins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Extractor de Datos de PDF',
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
