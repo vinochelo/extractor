8da2b975c7771ad1bc699200fc4b5d493aab4f2c
Rocku <vicio86.vicio@gmail.com>
2025-11-06 12:26:47 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  devIndicators: {
+    buildActivity: false,
+  },
+  // Adding allowedDevOrigins to solve the cross-origin warning
+  experimental: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1762372360685.cluster-lr6dwlc2lzbcctqhqorax5zmro.cloudworkstations.dev',
+    ],
+  },
 };
 
 export default nextConfig;
